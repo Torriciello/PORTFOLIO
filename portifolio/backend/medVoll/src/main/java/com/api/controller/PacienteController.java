c13a90cc3a9db3de6c37af7879b4a4d066009f8b
Torriciello <gabrielvictortorriciello@outlook.com>
2026-03-10 09:46:04 -0300

    correção

--- portifolio/backend/medVoll/src/main/java/com/api/controller/PacienteController.java
+++ portifolio/backend/medVoll/src/main/java/com/api/controller/PacienteController.java
@@ -1,6 +1,6 @@
 package com.api.controller;
 
-import org.springdoc.core.converters.models.Pageable;
+import org.springframework.data.domain.Pageable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.web.PageableDefault;
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.api.domain.patient.Patient;
