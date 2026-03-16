e8e045200117e223fd332629c6a75a650e797081
Gabriel Torriciello <gabrielvictortorriciello@outlook.com>
2026-03-16 08:54:01 -0300

    commit

--- portfolio_projects/frontend_projects/fokus/script.js
+++ portfolio_projects/frontend_projects/fokus/script.js
@@ -13,7 +13,7 @@ const tempoNaTela = document.querySelector('#timer')
 
 const musica = new Audio('sons/luna-rise-part-one.mp3')
 const audioPlay = new Audio('./sons/play.wav');
-const audioPausa = new Audio('.sons/pause.mp3');
+const audioPausa = new Audio('./sons/pause.mp3');
 const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
 let tempoDecorridoEmSegundos = 1500
