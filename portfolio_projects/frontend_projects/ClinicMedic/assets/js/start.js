ac2f948f636b4ff230f8379850da15e3a255484b
Gabriel Torriciello <gabrielvictortorriciello@outlook.com>
2026-03-20 14:46:33 -0300

    commit

--- portfolio_projects/frontend_projects/ClinicMedic/assets/js/start.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const tempoDeEspera = 1000;
-
-    setTimeout(() => {        
-        window.location.href = "../login/login.html";
-    }, tempoDeEspera);
-});
